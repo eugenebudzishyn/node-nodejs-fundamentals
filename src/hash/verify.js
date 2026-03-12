e4404b1c37900da2d74cd5d1dda7b8afb923adce
eugenebudzishyn <ybudzishyn@gmail.com>
2026-03-12 23:32:55 +0400

    taskDOne

--- src/hash/verify.js
+++ src/hash/verify.js
@@ -1,6 +1,5 @@
 import crypto from "crypto"
 import fs from "fs/promises"
-import stream from "stream"
 
 const verify = async () => {
   // Write your code here
@@ -8,10 +7,18 @@ const verify = async () => {
   // Calculate SHA256 hash using Streams API
   // Print result: filename — OK/FAIL
   try {
+
     const contents = JSON.parse(await fs.readFile("checksums.json"));
+    let actualHashes = {};
+
     for (let file in contents){
-      
+      actualHashes[file] = crypto.createHash("sha256").update(file).digest("hex");
     }
+
+    for (let file in actualHashes){
+      console.log(`${file} - ${actualHashes[file] === contents[file] ? "OK" : "FAIL"}`);
+    }
+
   } catch {
     throw new Error("FS operation failed");
   }
